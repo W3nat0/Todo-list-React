ca1bec4825712898dcf84731bbe81e372219f722
Arman <arman.2019.mkrtchyan@gmail.com>
2024-06-10 15:29:33 +0400

    fix: Edit button and scroll

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import "./Todo.css";
 
 class Todo extends Component {
@@ -8,17 +8,21 @@ class Todo extends Component {
     this.state = {
       title: "",
       tasks: savedTasks ? JSON.parse(savedTasks) : [],
+      editingTaskId: null,
+      editingTitle: "",
     };
   }
-
+  // save localStorage
   saveToLocalStorage(tasks) {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }
 
+  // on change
   handleOnChange = (e) => {
     this.setState({ title: e.target.value });
   };
 
+  // Submit button
   handleOnSubmit = (e) => {
     e.preventDefault();
     const { title, tasks } = this.state;
@@ -35,12 +39,7 @@ class Todo extends Component {
     }
   };
 
-  handleOnEdit = (id) => {
-    const taskToEdit = this.state.tasks.find((task) => task.id === id);
-    this.setState({ title: taskToEdit.title });
-    this.handleOnRemove(id);
-  };
-
+  // Remove button
   handleOnRemove = (id) => {
     const newTasks = this.state.tasks.filter((task) => task.id !== id);
     this.setState({
@@ -49,6 +48,7 @@ class Todo extends Component {
     this.saveToLocalStorage(newTasks);
   };
 
+  // Check button
   handleToggleChecked = (id) => {
     const newTasks = this.state.tasks.map((task) =>
       task.id === id ? { ...task, checked: !task.checked } : task
@@ -57,6 +57,41 @@ class Todo extends Component {
       tasks: newTasks,
     });
     this.saveToLocalStorage(newTasks);
+  };
+
+  // Edit button
+  handleEditChange = (e) => {
+    this.setState({ editingTitle: e.target.value });
+  };
+
+  handleOnEdit = (id) => {
+    const taskToEdit = this.state.tasks.find((task) => task.id === id);
+    this.setState({
+      editingTaskId: id,
+      editingTitle: taskToEdit.title,
+    });
+  };
+
+  handleEditSubmit = (id) => {
+    const { editingTitle, tasks } = this.state;
+    if (editingTitle.trim() !== "") {
+      const newTasks = tasks.map((task) =>
+        task.id === id ? { ...task, title: editingTitle } : task
+      );
+      this.setState({
+        tasks: newTasks,
+        editingTaskId: null,
+        editingTitle: "",
+      });
+      this.saveToLocalStorage(newTasks);
+    }
+  };
+
+  handleCancelEdit = () => {
+    this.setState({
+      editingTaskId: null,
+      editingTitle: "",
+    });
   };
 
   render() {
@@ -75,37 +110,67 @@ class Todo extends Component {
         <ul>
           {this.state.tasks.map((task) => (
             <li key={task.id}>
-              <div className="content">
-                <h3
-                  style={{
-                    textDecoration: task.checked ? "line-through" : "none",
-                    color: task.checked ? "grey" : "black",
-                    marginRight: "300px",
-                  }}
-                >
-                  {task.title}
-                </h3>
-                <div className="btn-container">
-                  <button
-                    onClick={() => this.handleToggleChecked(task.id)}
-                    className="check-btn"
-                  >
-                    <i className="fas fa-check"></i>
-                  </button>
-                  <button
-                    onClick={() => this.handleOnEdit(task.id)}
-                    className="edit-btn"
-                  >
-                    <i className="fas fa-edit"></i>
-                  </button>
-                  <button
-                    onClick={() => this.handleOnRemove(task.id)}
-                    className="remove-btn"
-                  >
-                    <i className="fas fa-trash"></i>
-                  </button>
+              {this.state.editingTaskId === task.id ? (
+                <div className="edit-task">
+                  <form>
+                    <input
+                      type="text"
+                      value={this.state.editingTitle}
+                      onChange={this.handleEditChange}
+                    />
+
+                    <button
+                      className="edit-save"
+                      type="submit"
+                      onClick={() => this.handleEditSubmit(task.id)}
+                    >
+                      Save
+                    </button>
+
+                    <button
+                      className="edit-cancel"
+                      onClick={this.handleCancelEdit}
+                    >
+                      Cancel
+                    </button>
+                  </form>
                 </div>
-              </div>
+              ) : (
+                <div className="content">
+                  <h3
+                    style={{
+                      textDecoration: task.checked ? "line-through" : "none",
+                      color: task.checked ? "grey" : "black",
+                      marginRight: "300px",
+                    }}
+                  >
+                    {task.title}
+                  </h3>
+
+                  <div className="btn-container">
+                    <button
+                      onClick={() => this.handleToggleChecked(task.id)}
+                      className="check-btn"
+                    >
+                      <i className="fas fa-check"></i>
+                    </button>
+
+                    <button
+                      onClick={() => this.handleOnEdit(task.id)}
+                      className="edit-btn"
+                    >
+                      <i className="fas fa-edit"></i>
+                    </button>
+
+                    <button
+                      onClick={() => this.handleOnRemove(task.id)}
+                      className="remove-btn"
+                    >
+                      <i className="fas fa-trash"></i>
+                    </button>
+                  </div>
+                </div>
+              )}
             </li>
           ))}
         </ul>
